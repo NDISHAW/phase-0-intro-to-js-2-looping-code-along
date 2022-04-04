41ee51ef93d5eed47a2844d8f176419ab1b3181c
NDICHU <ndichumuriithi@gmail.com>
2022-04-04 18:39:56 +0300

    ,Comleted asssignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+// const gifts =["teddybear","drone", "doll"];
+// function wrapGifts(gifts){
+// for (let i=0; i<gifts.length;i++){
+//     console.log(`wrapped ${gifts [i]} and added  abow!`);
+//     debugger;
+// }
+// return gifts;
+// }
+// wrapGifts(gifts);
+
+const names=["Guadalupe","Ollie", "Aki"];
+
+function writeCards(names, event="birthday") {
+let thanks=[]
+for(let i = 0; i < names.length; i++){
+    thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+}
+return thanks;
+};
+function countDown(startno){
+    while (startno>0) {
+        console.log(startno);
+        startno-=1;
+    }
+    console.log(startno);
+}
